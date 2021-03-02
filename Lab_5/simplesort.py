de14f46e5cfc3efec249c3c776fa59c84848cf6b
Liam Cannon <liamcannon@Liams-MacBook-Pro-2.local>
2021-03-01 20:29:45 -0500

    Cert auth

--- Lab_5/simplesort.py
+++ Lab_5/simplesort.py
@@ -1,4 +1,20 @@
-"""Don't forget your docstring!"""
+"""Liam Cannons Code for Lab 5.
+
+Author: Liam Cannon
+Class: CSI-275-01/02
+Assignment: Lab/HW 5 -- Sorting Server
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
 import socket
 HOST = "localhost"
 PORT = 20000
