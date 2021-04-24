3dddd8edfd0a1c372d457cf5bc0669fc64f37775
liamcannon <liam.cannon@mymail.champlain.edu>
2021-04-24 16:22:30 -0400

    started to impliment chatclient

--- final/chatclient.py
+++ final/chatclient.py
@@ -5,3 +5,40 @@ import json
 HOST = 'localhost'
 PORT = 7778
 
+username = ""
+
+send_data_sock = socket.socket()
+recv_data_sock = socket.socket()
+
+def send_data(data):
+    '''Handles sending all data'''
+    json_data = json.dumps(data).encode('utf-8')
+    full_data = len(json_data).to_bytes(4, 'big') + json_data
+    send_data_sock.sendall(full_data)
+
+def recv_data():
+    '''Handles Recieving all data from server'''
+    while True:
+        data_size = int.from_bytes(recv_data_sock.recv(4), 'big')
+        full_data = json.loads(recv_data_sock.recv(data_size).decode('utf-8'))
+        print(full_data)
+
+def chat_server():
+    try:
+        recv_data_sock.connect((HOST, PORT))
+        send_data_sock.connect((HOST, PORT))
+        data = ['START', username]
+        json_data = json.dumps(full_data).encode('utf-8')
+        full_data = len(json_data).to_bytes(4, 'big') + json_data
+        recv_data_sock.sendall(full_data)
+    except Exception:
+        recv_data_sock.close()
+        send_data_sock.close()
+        print('Closing connection')
+
+def get_chat():
+    
+
+
+if __name__ == "__main__":
+    
--- final/chatserver.py
+++ final/chatserver.py
@@ -8,9 +8,10 @@ PORT = 7778
 
 clients = {}
 
+
 def open_connection(data, sock):
     '''Opens the connection for the client
-    
+
     To be used when there is a new connection recieved 
     and assigns that connection to a new client
     '''
@@ -19,10 +20,10 @@ def open_connection(data, sock):
     print(username, " connected")
     clients[username] = sock
 
-    #dumping all data to json and encoding
-    json_data = json.dumps(username + "has connected").encode('utf-8')
+    # dumping all data to json and encoding
+    json_data = json.dumps(username + " has connected").encode('utf-8')
 
-    #sending the len of the message first per assignment requirments
+    # sending the len of the message first per assignment requirments
     data = len(json_data).to_bytes(4, 'big') + json_data
     # https://www.tutorialspoint.com/python/dictionary_items.htm
     for user, conn in clients.items():
@@ -39,65 +40,73 @@ def close_connection(data):
     clients[data[1]].close()
     del clients[data[1]]
     json_data = json.dumps(data[1] + " has disconnected").encode('utf-8')
-    data = len(json_data).to_bytes(4, 'big') + json_data
+    full_data = len(json_data).to_bytes(4, 'big') + json_data
 
     for user, conn in clients.items():
         try:
-            conn.sendall(data)
+            conn.sendall(full_data)
         except Exception:
-            del clients[user]  
+            del clients[user]
 
 
 def broadcast_message(data):
-    '''Handles Broadcasting messasges to all users'''
+    '''Handles Broadcasting messasges to all users
+
+    Client sends data like ['BROADCAST', Liam, 'test']
+    '''
     # The user sends an example of "[BROADCAST, Liam, 'test]"
     json_data = json.dumps(str(data[1]) + "> " + str(data[2])).encode('utf-8')
-    data = len(json_data).to_bytes(4, 'big') + json_data
+    full_data = len(json_data).to_bytes(4, 'big') + json_data
     for user, conn in clients.items():
         try:
-            conn.sendall(data)
+            conn.sendall(full_data)
         except Exception:
             del clients[user]
 
 
 def private_message(data):
-    # [Private, user1, message, user2]
-    json_data = json.dumps(str(data[1]) + '>' + str(data[2])).encode('utf-8')
-    data = len(json_data).to_bytes(4, 'big') + json_data
+    '''Handles sending private messages per user
 
+    the users sends data that looks like ['PRIVATE', user1, message, user2]
+    '''
+    # [Private, user1, message, user2]
+    json_data = json.dumps(str(data[1]) + '> ' + str(data[2])).encode('utf-8')
+    full_data = len(json_data).to_bytes(4, 'big') + json_data
     # sending to both users so they can both see it
     # uses two separate try excepts so if one user crashes/disconnects
     # it can be removed
+    errormsgdata = "Error user is offline"
     try:
-        clients[data[1]].sendall(data)
+        clients[data[3]].sendall(full_data)
+        clients[data[1]].sendall(full_data)
     except Exception:
-        del clients[data[1]]
-    try:
-        clients[data[3]].sendall(data)
-    except Exception:
-        del clients[data[3]]
+        try:
+            clients[1].sendall(errormsgdata)
+        except:
+            pass
 
 
 def handle_message(conn, addr):
+    ''' Handles sending out all messages '''
     try:
         while True:
             msg_size = int.from_bytes(conn.recv(4), 'big')
-            data = json.loads(conn.recv(msg_size).decode('utf-8'))
-            first_index = data[0].upper()
+            full_data = json.loads(conn.recv(msg_size).decode('utf-8'))
+            first_index = str(full_data[0]).upper()
             if first_index == 'START':
-                open_connection(data, conn)
+                open_connection(full_data, conn)
             elif first_index == 'EXIT':
-                close_connection(data)
+                close_connection(full_data)
             elif first_index == 'BROADCAST':
-                broadcast_message(data)
+                broadcast_message(full_data)
             elif first_index == 'PRIVATE':
-                broadcast_message(data)
+                private_message(full_data)
             else:
                 conn.close()
                 break
     except Exception:
         conn.close()
-                
+
 
 if __name__ == "__main__":
 
@@ -108,8 +117,9 @@ if __name__ == "__main__":
         try:
             client, addr = sock.accept()
             # creates a new thread for each client
+            # I could've used threading but _thread is simplier
             _thread.start_new_thread(handle_message, (client, addr))
         except:
             sock.close()
             print("Closing Down")
-            break    
+            break
